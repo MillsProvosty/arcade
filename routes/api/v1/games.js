4140118277360b60ffa9082447232bc2686e8e07
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-09-27 08:24:06 -0600

    Adds to readme

--- routes/api/v1/games.js
+++ routes/api/v1/games.js
@@ -15,4 +15,40 @@ router.get("/", function(req, res, next) {
     });
 });
 
+/*POST new game*/
+router.post("/", function(req, res, next) {
+  Game.create({
+          title: req.body.title,
+          price: req.body.price,
+          releaseYear: req.body.releaseYear,
+          active: req.body.active
+    })
+    .then(game => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(201).send(JSON.stringify(game));
+    })
+    .catch(error => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(500).send({ error });
+    });
+});
+
+/*PUT update game*/
+router.put("/", function(req, res, next) {
+  Game.update({
+          title: req.body.title,
+          price: req.body.price,
+          releaseYear: req.body.releaseYear,
+          active: req.body.active
+    })
+    .then(game => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(201).send(JSON.stringify(game));
+    })
+    .catch(error => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(500).send({ error });
+    });
+});
+
 module.exports = router; //this should stay at the bottom of the file
